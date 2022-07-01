29bb2726f2d0830dba6096d34ef575dc6a8c823c
Nikolay Suschenko <n.sushchenko@adventum.ru>
2022-07-01 16:55:09 +0300

    Added run statistics, action plan updated

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Experiments with random walks and pipeline to run them.
 ## Pipeline
 ![Pipeline diagram](docs/pipeline.svg)
 
+## Results file naming
+    <tag>.<experiment_number>.<hypotesis_number>-<implementation_number>.YYYYMMDDHHMMSS.<thread_id>
+
+For example:
+walk.2.3-1.20220301201003.d670460
+
 ## Action plan
-- [ ] Make hello world simulator
-- [ ] Calculate statistics (# of iterations, run time)
+- [x] Make hello world simulator
+- [x] Calculate statistics (# of iterations, run time)
+- [ ] Save results to file
+- [ ] Save results to Clickhouse
+- [ ] Save statistics to file
+- [ ] Save statistics to Clickhouse
--- simulator/hammy.cpp
+++ simulator/hammy.cpp
@@ -1,43 +1,72 @@
 #include <cassert>
-#include <cstdint>
-#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <time.h>
 #include <array>
 
-const unsigned EPOCH_LENGTH { 1'000'000 };
-const unsigned EPOCHS_TARGET_COUNT { 1'000 };
+using std::cout;
+using std::endl;
+
+const unsigned EPOCH_LENGTH { 1000'00 };
+const unsigned EPOCHS_TARGET_COUNT { 1 };
 using position_t = int;
 using epoch_t = std::array<position_t, EPOCH_LENGTH>;
 using simulation_t = std::array<epoch_t, EPOCHS_TARGET_COUNT>;
-const position_t TARGET_POSITION { 1'000 };
+const position_t TARGET_POSITION { 1000 };
 
 class RandomBitSource {
-	int64_t bits = rand();
-	int64_t bitMask = RAND_MAX;
-	static_assert((RAND_MAX ^ (RAND_MAX - 1)) == 1, "No support for RAND_MAX != 2^(n-1)");
+	int64_t m_bits {rand()};
+	int64_t m_bitMask {RAND_MAX};
+	static_assert((RAND_MAX ^ (RAND_MAX - 1)) == 1,
+			"No support for RAND_MAX != 2^(n-1)");
 public:
 	auto getBit() {
-		if (!bitMask) bits = rand(), bitMask = RAND_MAX;
-		bitMask >>= 1;
-		bits >>= 1;
-		return static_cast<bool>(bits & 1);
+		if (!m_bitMask)
+			m_bits = rand(), m_bitMask = RAND_MAX;
+		bool ret = m_bits & 1;
+		m_bitMask >>= 1;
+		m_bits >>= 1;
+		return ret;
+	}
+};
+
+class TimeMeasurer {
+	timespec m_beginRealTime;
+	timespec m_beginCpuTime;
+public:
+	TimeMeasurer() {
+		clock_gettime(CLOCK_MONOTONIC, &m_beginRealTime);
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &m_beginCpuTime);
+	}
+
+	auto getTime(bool cpuTime) const {
+		const timespec& beginTime {cpuTime ? m_beginCpuTime : m_beginRealTime};
+		timespec endTime;
+		clock_gettime(cpuTime ? CLOCK_PROCESS_CPUTIME_ID : CLOCK_MONOTONIC, &endTime);		
+		long seconds = endTime.tv_sec - beginTime.tv_sec;
+		long nanoseconds = endTime.tv_nsec - beginTime.tv_nsec;
+		return seconds + nanoseconds * 1e-9;
 	}
 };
 
 int main(int argc, char **argv) {
-	std::cout << "XXXs";
-	simulation_t simulation {};
+	TimeMeasurer const timeMeasurer;
+	simulation_t simulation;
 	RandomBitSource src;
-	for (auto& epoch : simulation) {
-		position_t position = 0;
+	size_t numIterations { };
+	for (auto &epoch : simulation) {
 		do {
-			for (auto& point : epoch) {
-				const int b {src.getBit()};
+			position_t position = 0;
+			for (auto &point : epoch) {
+				numIterations++;
+				const int b { src.getBit() };
 				position += b - ((~b) & 1);
 				point = position;
-			}			
-		}  while (epoch.back() != TARGET_POSITION);
-		std::cout << "Finished iteration" << std::endl;
+			}
+		} while (epoch.back() != TARGET_POSITION);
+		cout << "Finished iteration: " << numIterations << endl;
 	}
+	cout << "Wall time: " << timeMeasurer.getTime(false) << endl;
+	cout << "CPU time: " << timeMeasurer.getTime(true) << endl;
 	return 0;
 }
