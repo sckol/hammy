9b95a0f06cb1806a7d2ee2bde5a147556ec9e7fe
bimo <sckol@yandex.ru>
2024-02-20 07:00:00 +0300

    Исправлена ошибка при подсчете количества итераций

--- queries/count-targets.ch.sql
+++ queries/count-targets.ch.sql
@@ -3,5 +3,5 @@ insert into function s3(
   'CSVWithNames'
 ) 
 select target_position, count(*) cnt
-from s3('s3:///hammy/lagrangian/1/1/raw/lagrangian_*.gzip.parquet', Parquet)
+from s3('s3:///hammy/lagrangian/1/3/raw/lagrangian_*.gzip.parquet', Parquet)
 group by target_position
