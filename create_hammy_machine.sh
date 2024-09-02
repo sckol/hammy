c5f807350d6a051de4d19a882f64843c27db873a
bimo <sckol@yandex.ru>
2024-09-02 07:46:58 +0300

    Перенес hammy_maching.compose в папку docker

--- create_hammy_machine.sh
+++ create_hammy_machine.sh
@@ -4,7 +4,7 @@ if [[ $# -lt 2 ]] ; then
     echo 'Provide 2 arguments: version, number of minutes'
     exit 0
 fi
-sed  -e "s/XXX/$1/" -e "s/YYY/$2/" hammy_machine.compose > hammy_machine.compose.tmp
+sed  -e "s/XXX/$1/" -e "s/YYY/$2/" docker/hammy_machine.compose > hammy_machine.compose.tmp
 yc compute instance create-with-container \
   --cores 8 \
   --gpus 1 \
